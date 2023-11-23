7be863ff02f8e447f2d9187c69a40a44bdc9fd14
Aioria <None>
2023-11-23 20:32:25 +0800

    first commit

---
+++ mappings.lua
@@ -0,0 +1,46 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv-gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv-gv", { desc = "Move up" })
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+
+    -- navigate buffer tabs with `H` and `L`
+    -- L = {
+    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+    --   desc = "Next buffer",
+    -- },
+    -- H = {
+    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+    --   desc = "Previous buffer",
+    -- },
+
+    -- mappings seen under group name "Buffer"
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
+      end,
+      desc = "Pick to close",
+    },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
---
+++ plugins/community.lua
@@ -0,0 +1,21 @@
+return {
+    -- Add the community repository of plugin specifications
+    "AstroNvim/astrocommunity",
+    -- example of importing a plugin, comment out to use it or add your own
+    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+    -- { import = "astrocommunity.colorscheme.catppuccin" },
+    -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+    { import = "astrocommunity.indent.mini-indentscope" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.html-css" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.yaml" },
+    { import = "astrocommunity.pack.toml" },
+    { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.diagnostics.trouble-nvim" },
+}
